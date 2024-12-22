5d344bbc7f269ee468a7101909976edd683d91fc
Stedee <doanthanhbnh@gmail.com>
2024-12-22 23:19:51 +0700

    code cleaning

--- project/mods/stedee.WhatsBlockingMe/Scenes/skip_button.gd
+++ project/mods/stedee.WhatsBlockingMe/Scenes/skip_button.gd
@@ -1,20 +1,12 @@
 extends MarginContainer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# show after t in loading menu resets 5 times
 	if get_parent().b >= 5:
 		$VBoxContainer/HBoxContainer/Button.show()
 
-
 func _on_Button_pressed():
-	get_parent()._join_world() # Replace with function body.
+	get_parent()._join_world()
--- project/mods/stedee.WhatsBlockingMe/main.gd
+++ project/mods/stedee.WhatsBlockingMe/main.gd
@@ -2,20 +2,10 @@ extends Node
 
 const SkipButton := preload("res://mods/stedee.WhatsBlockingMe/Scenes/skip_button.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().connect("node_added", self, "_add_skip_button") # Replace with function body.
+	get_tree().connect("node_added", self, "_add_skip_button")
 
 func _add_skip_button(node: Node) -> void:
 	if node.name == "loading_menu":
 		var button: MarginContainer = SkipButton.instance()
 		node.add_child(button)
-			
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
